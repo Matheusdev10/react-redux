4679fa521ecb1230d21f786e1c8880a115859c42
Matheus <matheus_501@hotmail.com>
2023-12-12 16:13:40 -0300

    estilizando tabela do formUsers com material ui

--- src/components/TableUsers/index.tsx
+++ src/components/TableUsers/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { Users } from '../../pages/Users';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 export interface IUser {
   table: Array<Users>;
@@ -7,30 +15,32 @@ export interface IUser {
 
 const TableUsers: React.FC<IUser> = ({ table }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>nome</th>
-          <th>cpf</th>
-          <th>DataNascimento</th>
-          <th>email</th>
-          <th>senha</th>
-        </tr>
-      </thead>
-      <tbody>
-        {table.map(({ id, nome, cpf, dataNascimento, email, senha }) => (
-          <tr>
-            <td>{id}</td>
-            <td>{nome}</td>
-            <td>{cpf}</td>
-            <td>{dataNascimento}</td>
-            <td>{email}</td>
-            <td>{senha}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <TableContainer sx={{ marginTop: 4 }}>
+      <Table size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Id</TableCell>
+            <TableCell align="right">Nome</TableCell>
+            <TableCell align="right">Cpf</TableCell>
+            <TableCell align="right">Data de Nascimento</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Senha</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {table.map(({ id, nome, cpf, dataNascimento, email, senha }) => (
+            <TableRow key={id}>
+              <TableCell align="right">{id}</TableCell>
+              <TableCell align="right">{nome}</TableCell>
+              <TableCell align="right">{cpf}</TableCell>
+              <TableCell align="right">{dataNascimento}</TableCell>
+              <TableCell align="right">{email}</TableCell>
+              <TableCell align="right">{senha}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 

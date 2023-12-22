6d8dcedc421badf19bf4339ed57b6e6cffd2eab5
Matheus <matheus_501@hotmail.com>
2023-12-22 00:21:14 -0300

    trabalhando na funcao de ordenação pelo nome na tabela de formalização

--- src/components/TableFormalization/index.tsx
+++ src/components/TableFormalization/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { format, addHours } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
 import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
@@ -21,14 +23,31 @@ export const TableFormalization: React.FC<ITableFormalizations> = ({
   table,
 }) => {
   const regexCpf = /(\d{3})(\d{3})(\d{3})(\d{2})/;
-
   const regexCnpj = /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/;
+
+  const [ordenar, setOrdenar] = useState<Array<IFormalization>>(table);
+  const [order, setOrder] = useState('ASC');
+
+  function handleOrdenarNome() {
+    const ordenarNome = [...ordenar];
+    ordenarNome.sort(function (a, b) {
+      return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+    });
+    setOrdenar(ordenarNome);
+    console.log(ordenarNome);
+  }
   return (
     <TableContainer sx={{ marginTop: 4 }}>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell align="center">nome</TableCell>
+            <TableCell
+              onClick={handleOrdenarNome}
+              sx={{ cursor: 'pointer' }}
+              align="center"
+            >
+              nome
+            </TableCell>
             <TableCell align="center">cpf/cnpj</TableCell>
             <TableCell align="center">produto</TableCell>
             <TableCell align="center">data</TableCell>
@@ -46,11 +65,15 @@ export const TableFormalization: React.FC<ITableFormalizations> = ({
                 <TableCell align="center">
                   {cpfCnpj.length === 11
                     ? cpfCnpj.replace(regexCpf, '$1.$2.$3-$4')
-                    : 'ola'}
+                    : cpfCnpj.replace(regexCnpj, '$1.$2.$3/$4-$5')}
                 </TableCell>
 
                 <TableCell align="center">{produto}</TableCell>
-                <TableCell align="center">{data}</TableCell>
+                <TableCell align="center">
+                  {format(data, 'dd/MM/yyyy HH:mm', {
+                    locale: ptBR,
+                  })}
+                </TableCell>
                 <TableCell align="center">{tipoCadastro}</TableCell>
                 <TableCell align="center">{status}</TableCell>
                 <TableCell
